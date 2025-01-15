422f9927168b1ed049737d48d3a85fa04a9dc2f0
hakim hayate <hakimhayate@gmail.com>
2025-01-15 14:41:29 +0100

    commit

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,20 @@
 package personnages;
 
 public class Gaulois {
-
+	private String name;
+	private int force;
+	
+	public Gaulois(String name, int force) {
+		this.name = name;
+		this.force = force;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void parler(String text) {
+		System.out.println("Le gaulois " + this.name +" : \""+text+"\"");;
+	}
 }
---
+++ src/test_fonctionnel/TestGaulois.java
@@ -0,0 +1,9 @@
+package test_fonctionnel;
+import personnages.*;
+public class TestGaulois {
+	// main A
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		asterix.parler("Bonjour");
+	}
+}
